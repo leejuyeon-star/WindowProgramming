3b2029df9819764a2ed90292c80e656d1aa6b0f2
juyeon Lee <76759160+leejuyeon-star@users.noreply.github.com>
2022-10-06 19:51:57 +0900

    Update ChildView.h

--- Simple/ChildView.h
+++ Simple/ChildView.h
@@ -33,14 +33,19 @@ protected:
 	afx_msg void OnPaint();
 	DECLARE_MESSAGE_MAP()
 public:
-//	afx_msg void OnClose();
-//	afx_msg void OnClose();
-//	afx_msg void OnDestroy();
-//	afx_msg void OnClose();
-//	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
-//	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
-//	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
 	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+	BOOL m_bDrawMode;	//그리기 작업이 진행중임을 나타냄
+	int m_x1, m_y1, m_x2, m_y2;		//타원에 외접하는 직사각형의 최상단, 우하단 죄표
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg BOOL OnSetCursor(CWnd* pWnd, UINT nHitTest, UINT message);
+	int m_xPos, m_yPos;	//도형의 현재 위치
+	int m_xMax, m_yMax;	//클라이언트 영역의 크기
+	BOOL m_bFill;	//도형의 내부를 채울지 여부
+	CArray<CPoint, CPoint&> circleArr;
+	int circleCount;
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
 };
 
