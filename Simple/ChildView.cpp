201a9937cd07bc2bd384081ecc120f1360b847d9
juyeon Lee <76759160+leejuyeon-star@users.noreply.github.com>
2022-10-06 19:52:33 +0900

    Update ChildView.cpp

--- Simple/ChildView.cpp
+++ Simple/ChildView.cpp
@@ -16,6 +16,27 @@
 
 CChildView::CChildView()
 {
+	m_xPos = m_yPos = 60;
+	m_bFill = FALSE;
+	m_bDrawMode = false;
+
+	circleArr.SetSize(20);
+	circleCount = 0;
+
+	//CUIntArray array;
+	//array.SetSize(10);
+	//array[1];
+
+	//array.InsertAt(인덱스, 값);
+	//array.RemoveAt(인덱스);
+	//array.AddTail(값);
+
+	//CArray<Point3D, Point3D&> array;		
+
+
+
+	//CList도 있다. (97p)(포인터로 연결되어 앞으로넣기, 뒤로넣기 기능 있음, 리스트 크기지정안해도됨)
+	
 }
 
 CChildView::~CChildView()
@@ -25,15 +46,13 @@ CChildView::~CChildView()
 
 BEGIN_MESSAGE_MAP(CChildView, CWnd)
 	ON_WM_PAINT()
-//	ON_WM_CLOSE()
-//ON_WM_CLOSE()
-//ON_WM_DESTROY()
-//ON_WM_CLOSE()
-//ON_WM_LBUTTONDOWN()
-//ON_WM_RBUTTONDOWN()
 ON_WM_LBUTTONDOWN()
-//ON_WM_ERASEBKGND()
 ON_WM_RBUTTONDOWN()
+ON_WM_MOUSEMOVE()
+ON_WM_LBUTTONUP()
+ON_WM_SETCURSOR()
+ON_WM_SIZE()
+ON_WM_KEYDOWN()
 END_MESSAGE_MAP()
 
 
@@ -49,33 +68,42 @@ BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
 	cs.style &= ~WS_BORDER;
 
 	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
-		::LoadCursor(nullptr, IDC_ARROW), (HBRUSH)GetStockObject(GRAY_BRUSH), nullptr);
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1), nullptr);
 	return TRUE;
 }
 
 void CChildView::OnPaint() 
 {
+	CPaintDC dc(this);
 
+	CRect rect;
+	GetClientRect(&rect);
 
+	if(m_bDrawMode)
+		dc.Ellipse(m_x1-20, m_y1-20, m_x1 + 20, m_y1 + 20);
 
-	CPaintDC dc(this); // 그릴 수 있는 디바이스 컨텍스트
-	dc.TextOut(100, 50, CString(" OPAQUE 모드 [1] "));
-	dc.SetBkMode(TRANSPARENT);
-	dc.TextOut(100, 100, CString(" TRANSPARENT 모드 "));
-	dc.SetBkMode(OPAQUE);
-	dc.SetBkColor(RGB(0, 255, 0)); // 
-	dc.TextOut(100, 150, CString(" OPAQUE 모드 [2] "));
+	if (circleCount)
+	{
+		for (int i = 0; i < circleCount; i++)
+		{
+			dc.Ellipse(circleArr[i].x - 20, circleArr[i].y - 20, circleArr[i].x + 20, circleArr[i].y + 20);
+		}
+	}
+
 	
-	
+
 	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
 }
 
 void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
 {
-	CPaintDC dc(this); // 그릴 수 있는 디바이스 컨텍스트
-	dc.SetBkMode(OPAQUE); // 배경모드 불투명
-	Invalidate(false);		//화면 갱신
-	dc.SetBkColor(RGB(10, 250, 0)); // 배경색 설정
+	m_bDrawMode = true;
+	m_x1 = point.x;
+	m_y1 = point.y;
+	
+	circleArr.InsertAt(circleCount, point);
+	circleCount++;
+	Invalidate();
 
 	CWnd::OnLButtonDown(nFlags, point);
 }
@@ -84,10 +112,71 @@ void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
 
 void CChildView::OnRButtonDown(UINT nFlags, CPoint point)
 {
-	
-	CPaintDC dc(this); // 그릴 수 있는 디바이스 컨텍스트
-	dc.SetBkMode(TRANSPARENT); // 배경모드 투명
-	Invalidate(false);		//화면 갱신
+
+
 	
 	CWnd::OnRButtonDown(nFlags, point);
+}
+
+
+void CChildView::OnMouseMove(UINT nFlags, CPoint point)
+{
+
+}
+
+
+void CChildView::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	m_bDrawMode = false;
+
+	CWnd::OnLButtonUp(nFlags, point);
+}
+
+
+BOOL CChildView::OnSetCursor(CWnd* pWnd, UINT nHitTest, UINT message)
+{
+
+	return CWnd::OnSetCursor(pWnd, nHitTest, message);
+}
+
+
+
+
+
+
+
+void CChildView::OnSize(UINT nType, int cx, int cy)
+{
+	CWnd::OnSize(nType, cx, cy);
+
+	m_xMax = cx;
+	m_yMax = cy;
+}
+
+
+void CChildView::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	switch (nChar) {
+	case VK_LEFT:
+		m_xPos -= 20; 
+		break;
+	case VK_RIGHT: 
+		m_xPos += 20; 
+		break;
+	case VK_UP: 
+		m_yPos -= 20; 
+		break;
+	case VK_DOWN: 
+		m_yPos += 20; 
+		break;
+	case VK_SPACE: 
+		m_bFill = !m_bFill;
+	}
+	// 20 <= m_xPos <= m_xMax-20
+	m_xPos = min(max(20, m_xPos), m_xMax - 20); 
+	// 20 <= m_yPos <= m_yMax-20
+	m_yPos = min(max(20, m_yPos), m_yMax - 20); 
+	Invalidate();
+
+	CWnd::OnKeyDown(nChar, nRepCnt, nFlags);
 }
