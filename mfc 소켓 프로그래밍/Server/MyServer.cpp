2757f6f174a43d4f2f067866cfefe51b0a294c5a
juyeon Lee <76759160+leejuyeon-star@users.noreply.github.com>
2022-10-16 23:43:22 +0900

    Add files via upload

---
+++ mfc 소켓 프로그래밍/Server/MyServer.cpp
@@ -0,0 +1,29 @@
+﻿// MyServer.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ValueServer.h"
+#include "MyServer.h"
+
+
+// MyServer
+
+MyServer::MyServer()
+{
+}
+
+MyServer::~MyServer()
+{
+}
+
+
+// MyServer 멤버 함수
+
+//클라이언트가 접속 시도시 호출됨
+void MyServer::OnAccept(int nErrorCode)
+{
+	//해당 클라이언트와만 연결할 소켓 생성
+	Accept(m_user);
+
+	CSocket::OnAccept(nErrorCode);
+}
---
+++ mfc 소켓 프로그래밍/Server/MyUser.cpp
@@ -0,0 +1,46 @@
+﻿// MyUser.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ValueServer.h"
+#include "MyUser.h"
+
+
+// MyUser
+
+MyUser::MyUser()
+{
+}
+
+MyUser::~MyUser()
+{
+}
+
+
+// MyUser 멤버 함수
+
+
+//클라이언트에게 수신시 호출
+void MyUser::OnReceive(int nErrorCode)
+{
+	//받은 값을 int형(4byte) 크기로 받아 value에 받는다.
+	int value;
+	Receive(&value, sizeof(int));
+
+	//값 대화상자에 출력
+	AfxGetMainWnd()->SetDlgItemInt(IDC_VALUE_EDIT, value);
+	CSocket::OnReceive(nErrorCode);
+
+	//클라이언트에게 확인 답장
+	value = 1;
+	Send(&value, sizeof(int));
+}
+
+
+//연결된 소켓이 닫히면(클라이언트가 종료하면) 호출됨
+void MyUser::OnClose(int nErrorCode)
+{
+	ShutDown(2);	//데이터 전송/수신하고 있는데 접속 끊는 함수 
+	Close();
+	CSocket::OnClose(nErrorCode);
+}
